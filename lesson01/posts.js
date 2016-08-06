9d59a506b175c3ad3fa4a7e15e587cb80ca5a9ac
Artem Zhukov <atem085@gmail.com>
2016-08-06 14:44:21 +0300

    Task is ready, but empty rows aren't remove because function 'continue' isn't work

--- lesson01/posts.js
+++ lesson01/posts.js
@@ -1,91 +1,37 @@
-$( document ).ready( function () {
+$(document).ready(function () {
     var posts = Data.getPosts();
 
-    var selectedPage = 0;
-    var perPage = 13;
-    var template = Handlebars.compile('post description - {{post.description}}');
+    var postsRow = $('#posts-json-template').html();
+    var postsRowTemplate = Handlebars.compile(postsRow);
 
-    var html = template({
-        post: Data.getPost()
+    var postsTable = $('#posts-table-template').html();
+    var postsTableTemplate = Handlebars.compile(postsTable);
+
+    Handlebars.registerHelper('json', function (array) {
+        return JSON.stringify(array, "", 5);
     });
 
-    $('.posts-container__list').html(html);
+    Handlebars.registerHelper('table', function (array, values) {
+        var string;
+        for (var i = 0; i < array.length; i++) {
 
-    console.log(html);
+            string = '<li>' + values.fn(array[i]);
 
+            if (values.fn(array[i]).length === 9) continue; //Хотел сделать пропуск пустых строк, но continue не срабатывает
 
-
-    var source = $(".posts-json").html();
-  /*  var template = Handlebars.compile(source);*/
-
-
-    Handlebars.registerHelper('json', function () {
-        console.log('JSON ');
-        return '';
-    });
-
-    var html = template(posts.description)
-
-    $('.posts-json').html(html);
-
-
-    /*    render();
-        subscribeHandlers();*/
-
-    function render() {
-        renderPosts();
-        renderNavigation();
-    }
-
-    function subscribeHandlers() {
-        $( '.posts-container__navigation' ).click( function( event ) {
-            var selected = parseInt( event.target.textContent ) - 1;
-            //var selected = parseInt($(event.target).data('id')) - 1;
-
-            if ( selected === selectedPage ) {
-                return;
-            }
-            selectedPage = selected;
-            renderPosts();
-            renderNavigation();
-            $( 'html,body' ).animate( { scrollTop : 0 }, 0 );
-        });
-
-        $( '.posts-container__post' ).click( function () {
-            console.log( 'selected post' );
-        } );
-    }
-
-    function renderNavigation() {
-        var count = Math.ceil( posts.length / perPage );
-        var numbContainer = $('<div></div>' ).addClass( '.posts-container__navigation-numbers' );
-
-        for ( var i = 0; i < count; i ++ ) {
-            var numberElement = $( '<div>' + ( i + 1 ) + '</div>' );
-            numberElement.addClass( 'posts-container__navigation-number' );
-            if ( i === selectedPage ) {
-                numberElement.addClass( 'posts-container__navigation-number_selected' );
-            }
-            numbContainer.append( numberElement );
         }
-        $( '.posts-container__navigation' ).html( numbContainer.html() );
-    }
+        return '<ul>' + string + '<li>'  + values.fn(array[i])  + '</li>'+ '</ul>';
+    });
 
-    function renderPosts() {
-        var postsForRender = posts.slice( selectedPage * perPage, selectedPage * perPage + perPage );
-        var container = $( '<div></div>' );
 
-        container.append( postsForRender.map( function ( post ) {
-            var postElem = $( '<div></div>' ).addClass( 'posts-container__post' );
-            var postIsland = $( '<div></div>' ).addClass( 'posts-container__post-island' );
+    var result = postsRowTemplate({posts: posts});
+    $('.posts-json').html('<pre>' + result + '</pre>');
 
-            postIsland.append( '<div class="posts-container__post-img-wrapper"><img class=posts-container__post-img src="' + post.imgUrl + '"></img></div>' );
-            postIsland.append( '<div class="posts-container__post-description">' + post.description + '</div>' );
-            postElem.append( postIsland );
+    var outHtml = (postsTableTemplate({posts: posts}));
+    $('.posts-table').html(outHtml);
 
-            return postElem;
-        } ) );
-        $( '.posts-container__list' ).html( container.html() );
-    }
+    $('ul').attr('type','none');
+    $('li:nth-child(odd)').attr('style','background:grey; padding:10px 0 10px 0');
+    $('li:nth-child(even)').attr('style','background:white; padding:10px 0 10px 0');
+
 });
-
