c151efd6c13ea4f6356f438a25f1ea0f8ea1f721
Artem Zhukov <atem085@gmail.com>
2016-09-12 22:16:12 +0300

    fix god-gift-form.js

--- lesson06/client_src/containers/god-gift-form/god-gift-form.js
+++ lesson06/client_src/containers/god-gift-form/god-gift-form.js
@@ -1,6 +1,6 @@
 var GiftTunner = require('containers/gift-tunner/gift-tunner.js');
 var GodHateIndicator = require('containers/god-hate-indicator/god-hate-indicator.js');
-var Resource  = require('models/resource.js');
+var Resource = require('models/resource.js');
 var Hate = require('models/hate.js');
 
 module.exports = function GodGiftForm(options) {
@@ -8,23 +8,31 @@ module.exports = function GodGiftForm(options) {
 
 
     var BASE_HATE = 30;
-    var userGoldResource = options.resources[0];
+    /*    var userGoldResource = options.resources[0];*/
     var BASE_GOLD = options.resources[0].getCount();
-    var userCopperResource = options.resources[1];
+    /*    var userCopperResource = options.resources[1];*/
     var BASE_COPPER = options.resources[1].getCount();
-    var userSomeResource = options.resources[2];
+    /* var userSomeResource = options.resources[2];*/
     var BASE_SOME = options.resources[2].getCount();
 
     var hate = new Hate(BASE_HATE);
-    var goldGiftResource = new Resource({name: 'Gold'});
-    var cooperGiftResource = new Resource({name: 'Copper'});
-    var someGiftResource = new Resource({name: 'Some'});
 
-    Model.subscribeAll([goldGiftResource, cooperGiftResource, someGiftResource], function() {
-        hate.setCount(BASE_HATE - someGiftResource.getCount() * 1 - cooperGiftResource.getCount() * 2  - goldGiftResource.getCount() * 3);
-        userGoldResource.setCount(BASE_GOLD - goldGiftResource.getCount());
-        userCopperResource.setCount(BASE_COPPER - cooperGiftResource.getCount());
-        userSomeResource.setCount(BASE_SOME - someGiftResource.getCount());
+    /*
+     var goldGiftResource = new Resource({name: 'Gold'});
+     var cooperGiftResource = new Resource({name: 'Copper'});
+     var someGiftResource = new Resource({name: 'Some'});*/
+
+
+    var resources = options.resources.map(function (res) {
+        return new Resource({name: res.getName()});
+    });
+
+
+    Model.subscribeAll(resources, function () {
+        hate.setCount(BASE_HATE - resources[2].getCount() * 1 - resources[1].getCount() * 2 - resources[0].getCount() * 3);
+        resources[0].setCount(BASE_GOLD - resources[0].getCount());
+        resources[1].setCount(BASE_COPPER - resources[1].getCount());
+        resources[2].setCount(BASE_SOME - resources[2].getCount());
     });
 
     var godHateIndicator = new GodHateIndicator({
@@ -32,16 +40,43 @@ module.exports = function GodGiftForm(options) {
     });
 
     var goldTunner = new GiftTunner({
-        resource: goldGiftResource
+        resource: resources[0]
     });
 
     var copperTunner = new GiftTunner({
-        resource: cooperGiftResource
+        resource: resources[1]
     });
 
     var someTunner = new GiftTunner({
-        resource: someGiftResource
+        resource: resources[2]
     });
+
+
+    /*
+     Model.subscribeAll([goldGiftResource, cooperGiftResource, someGiftResource], function() {
+     hate.setCount(BASE_HATE - someGiftResource.getCount() * 1 - cooperGiftResource.getCount() * 2  - goldGiftResource.getCount() * 3);
+     userGoldResource.setCount(BASE_GOLD - goldGiftResource.getCount());
+     userCopperResource.setCount(BASE_COPPER - cooperGiftResource.getCount());
+     userSomeResource.setCount(BASE_SOME - someGiftResource.getCount());
+     });
+
+     var godHateIndicator = new GodHateIndicator({
+     hate: hate
+     });
+
+     var goldTunner = new GiftTunner({
+     resource: goldGiftResource
+     });
+
+     var copperTunner = new GiftTunner({
+     resource: cooperGiftResource
+     });
+
+     var someTunner = new GiftTunner({
+     resource: someGiftResource
+     });
+     */
+
 
     function render() {
         elem.html(App.templates['god-gift-form']({}));
@@ -57,7 +92,7 @@ module.exports = function GodGiftForm(options) {
     }
 
     function subscribeHandlers(elem) {
-        elem.find('.god-gift-form__send').click(function() {
+        elem.find('.god-gift-form__send').click(function () {
             console.log('send gift [gold: ' + goldTunner.getCount() + ', copper:' + copperTunner.getCount() + ', some:' + someTunner.getCount() + ']');
         });
     }
